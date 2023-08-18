42d6db3f937c36f662a240b5194414f4dc7365b7
Colin Jackson <colin.jackson@shopify.com>
2023-08-18 04:08:57 -0700

    add advanced git search

---
+++ config_nvim/lua/plugins/advanced-git-search.lua
@@ -0,0 +1,14 @@
+return {
+	"aaronhallaert/advanced-git-search.nvim",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"tpope/vim-fugitive",
+		"tpope/vim-rhubarb",
+	},
+	keys = {
+		{ "<leader>g", "<cmd>AdvancedGitSearch search_log_content_file<cr>" },
+	},
+	config = function()
+		require("telescope").load_extension("advanced_git_search")
+	end,
+}
