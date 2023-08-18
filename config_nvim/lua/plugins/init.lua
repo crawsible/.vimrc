680a53e1e879c23a3aacebd69f255fcfc77b36d7
Colin Jackson <colin.jackson@shopify.com>
2023-08-18 02:18:54 -0700

    add copilot and move around keybindings to compensate

--- config_nvim/lua/plugins/init.lua
+++ config_nvim/lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
 	"AndrewRadev/tagalong.vim",
 	"folke/neodev.nvim",
+	"github/copilot.vim",
 	"lukas-reineke/lsp-format.nvim",
 	"mhinz/vim-signify",
 	"neovim/nvim-lspconfig",
--- config_nvim/lua/plugins/nvim-cmp.lua
+++ config_nvim/lua/plugins/nvim-cmp.lua
@@ -43,7 +43,7 @@ return {
 						fallback()
 					end
 				end),
-				["<Tab>"] = cmp.mapping(function(fallback)
+				["<A-Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
 					elseif luasnip.expand_or_jumpable() then
@@ -52,7 +52,7 @@ return {
 						fallback()
 					end
 				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
+				["<S-A-Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_prev_item()
 					elseif luasnip.jumpable(-1) then
